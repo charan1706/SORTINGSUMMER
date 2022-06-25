b7bb6715a652b8c32ba5f616863c55ec6584be01
charan <nlvscharan@gmail.com>
2022-06-25 15:59:23 +0530

    Bubble Sort

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,22 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class ReverseNumberExample1   
+		{  
+		public static void main(String[] args)   
+		{  
+		int number = 987654, reverse = 0;  
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);  
+		}  
+		}  
+	}
+
+}
